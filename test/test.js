fd0e7e32b68d6efae9283ccbacac38164d48c060
Dragan Nikolic <dragan@fintechdev.net>
2017-02-08 12:00:38 -0800

    add first tests :)

---
+++ test/test.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+
+  describe('#indexOf()', function() {
+    it('should return index when the value is present', function() {
+      assert.equal(1, [1,2,3].indexOf(2));
+    });
+  });
+});
